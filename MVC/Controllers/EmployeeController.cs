69c7588322eb4ce3443797626f3810f1c6dc5402
Hema Latha <Hema Latha@HEMALATHA-PC>
2020-08-31 13:11:07 +0530

    Add project files.

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using MVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            IEnumerable<MVCEmployeeModel> empList;
+            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee").Result;
+            empList = response.Content.ReadAsAsync<IEnumerable<MVCEmployeeModel>>().Result;
+            return View(empList);
+        }
+        public ActionResult AddOrEdit(int id=0)
+        {
+            if(id==0)
+                return View(new MVCEmployeeModel());
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Employee/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<MVCEmployeeModel>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(MVCEmployeeModel emp)
+        {
+            if (emp.ID == 0)
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PostAsJsonAsync("Employee", emp).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = GlobalVariables.WebApiClient.PutAsJsonAsync("Employee/"+ emp.ID,emp).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = GlobalVariables.WebApiClient.DeleteAsync("Employee/"+id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC/Models/MVCEmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class MVCEmployeeModel
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="This field is required")]
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
